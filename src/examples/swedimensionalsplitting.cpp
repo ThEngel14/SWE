10ab2ecb63f3ff1744e841e3eb50a705942ab931
kyuq <kevin.yu@tum.de>
2014-11-05 15:17:55 +0100

    delete SWE_DimensionalSPlitting from src/blocks and added it to the src/examples

---
+++ src/examples/swedimensionalsplitting.cpp
@@ -0,0 +1,14 @@
+/*
+ * swedimensionalsplitting.cpp
+ *
+ *  Created on: Nov 5, 2014
+ *      Author: kyu
+ */
+
+#include "swedimensionalsplitting.h"
+
+swe_dimensionalsplitting::swe_dimensionalsplitting() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ src/examples/swedimensionalsplitting.h
@@ -0,0 +1,16 @@
+/*
+ * swedimensionalsplitting.h
+ *
+ *  Created on: Nov 5, 2014
+ *      Author: kyu
+ */
+
+#ifndef SWEDIMENSIONALSPLITTING_H_
+#define SWEDIMENSIONALSPLITTING_H_
+
+class swe_dimensionalsplitting {
+public:
+	swe_dimensionalsplitting();
+};
+
+#endif /* SWEDIMENSIONALSPLITTING_H_ */
