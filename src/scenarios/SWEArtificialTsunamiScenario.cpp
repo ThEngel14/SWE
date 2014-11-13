e56ea5681177cf2a51a58cce6957bf8f3dc52d9e
kyuq <kevin.yu@tum.de>
2014-11-13 15:19:56 +0100

    initial artificialtsunami

---
+++ src/scenarios/SWEArtificialTsunamiScenario.cpp
@@ -0,0 +1,14 @@
+/*
+ * SWEArtificialTsunamiScenario.cpp
+ *
+ *  Created on: Nov 13, 2014
+ *      Author: kyu
+ */
+
+#include "SWEArtificialTsunamiScenario.h"
+
+SWE_ArtificialTsunamiScenario::SWE_ArtificialTsunamiScenario() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ src/scenarios/SWEArtificialTsunamiScenario.h
@@ -0,0 +1,22 @@
+/*
+ * SWEArtificialTsunamiScenario.h
+ *
+ *  Created on: Nov 13, 2014
+ *      Author: kyu
+ */
+
+#ifndef SWEARTIFICIALTSUNAMISCENARIO_H_
+#define SWEARTIFICIALTSUNAMISCENARIO_H_
+
+#include "SWE_Scenario.hh"
+
+class SWE_ArtificialTsunamiScenario: public SWE_Scenario {
+public:
+	SWE_ArtificialTsunamiScenario();
+	float getWaterHeight(float x, float y)
+
+	float getBathymetry(float x, float y)
+
+};
+
+#endif /* SWEARTIFICIALTSUNAMISCENARIO_H_ */
